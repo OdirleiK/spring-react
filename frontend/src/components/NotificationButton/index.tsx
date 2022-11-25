89944200da4431388c3f541b419c21ee349c6c73
Odirlei Oliver Kempfer Siqueira Junior <odirlei67777@gmail.com>
2022-11-25 13:10:41 -0300

    notificacao toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.png'
 import { BASE_URL } from '../../utils/request';
 
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        console.log("sucesso, id: " + id);
+       toast.info("SMS enviado com sucesso");
     })
 }
 
